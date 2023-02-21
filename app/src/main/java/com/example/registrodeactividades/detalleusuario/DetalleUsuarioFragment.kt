5a6caca3819041a117f473eb16aaf23f03a982e4
JhohellsDL <jhohellserick@gmail.com>
2023-02-21 16:20:01 -0500

    Observer retirado, lambda afuera

--- app/src/main/java/com/example/registrodeactividades/detalleusuario/DetalleUsuarioFragment.kt
+++ app/src/main/java/com/example/registrodeactividades/detalleusuario/DetalleUsuarioFragment.kt
@@ -6,8 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.contadorcasino.database.HijosDataBase
@@ -41,25 +39,26 @@ class DetalleUsuarioFragment : Fragment() {
 
         //*********** PARA NAVEGACION *****************************
         detalleUsuarioViewModel.idUserForNavigation.observe(
-            viewLifecycleOwner,
-            Observer { id ->
-                id?.let {
+            viewLifecycleOwner) {
+                it?.let {
                     this.findNavController().navigate(
                         DetalleUsuarioFragmentDirections.actionDetalleUsuarioFragmentToRegistroPorUsuarioFragment(
-                            id
+                            it
                         )
                     )
                     detalleUsuarioViewModel.onUserClickedNavigated()
                 }
-            })
+            }
         //**********************************************************
 
         binding.detalleRecyclerview.adapter = adapter
 
-        detalleUsuarioViewModel.hijos.observe(viewLifecycleOwner, Observer {
+        detalleUsuarioViewModel.hijos.observe(
+            viewLifecycleOwner,
+        ) {
             adapter.data = it
             Log.i("hijo", "Lista : $it")
-        })
+        }
         //-----------------------------------------------------------------------------------------------------------------------------------
 
         return binding.root
