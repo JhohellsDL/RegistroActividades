fed2fa4ba80a85a1cc48f891d70c097a70e9e214
JhohellsDL <jhohellserick@gmail.com>
2023-02-25 16:37:58 -0500

    add card alert

--- app/src/main/java/com/example/registrodeactividades/detalleusuario/DetalleUsuarioFragment.kt
+++ app/src/main/java/com/example/registrodeactividades/detalleusuario/DetalleUsuarioFragment.kt
@@ -1,19 +1,26 @@
 package com.example.registrodeactividades.detalleusuario
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.contadorcasino.database.HijosDataBase
+import com.example.registrodeactividades.databinding.CardAlertReiniciarBinding
 import com.example.registrodeactividades.databinding.FragmentDetalleUsuarioBinding
 
 class DetalleUsuarioFragment : Fragment() {
 
     private lateinit var binding: FragmentDetalleUsuarioBinding
+    private lateinit var bindingDialog: CardAlertReiniciarBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -60,6 +67,20 @@ class DetalleUsuarioFragment : Fragment() {
         }
         //-----------------------------------------------------------------------------------------------------------------------------------
 
+        binding.buttonReiniciar.setOnClickListener {
+            bindingDialog = CardAlertReiniciarBinding.inflate(layoutInflater)
+            val myDialog = Dialog(it.context)
+            myDialog.setContentView(bindingDialog.root)
+            myDialog.setCancelable(true)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialog.show()
+
+            bindingDialog.buttonReiniciar.setOnClickListener {
+                detalleUsuarioViewModel.onReinicioRegistroHijos()
+                myDialog.dismiss()
+                Toast.makeText(requireContext(),"Reiniciado",Toast.LENGTH_SHORT).show()
+            }
+        }
         return binding.root
     }
 }
