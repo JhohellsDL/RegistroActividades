96b19ec129da157164bf994cd4619782d5f914b1
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2023-10-08 16:55:35 -0500

    para notificaciones

--- app/src/main/java/com/example/registrodeactividades/MainActivity.kt
+++ app/src/main/java/com/example/registrodeactividades/MainActivity.kt
@@ -1,18 +1,73 @@
 package com.example.registrodeactividades
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.registrodeactividades.DataResetReceiver.Companion.NOTIFICATION_ID
 import com.example.registrodeactividades.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("ScheduleExactAlarm")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        createChannel()
+        scheduleNotification()
+    }
 
+    @SuppressLint("ScheduleExactAlarm")
+    private fun scheduleNotification() {
+        val intent = Intent(applicationContext, DataResetReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            NOTIFICATION_ID,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
 
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = System.currentTimeMillis()
+        calendar.set(Calendar.HOUR_OF_DAY, 14)
+        calendar.set(Calendar.MINUTE, 55)
+
+        alarmManager.setExact(
+            AlarmManager.RTC_WAKEUP,
+            Calendar.getInstance().timeInMillis + 10000,
+            pendingIntent
+        )
+    }
+
+    fun createChannel() {
+        val channelId = "myChannel" // Identificador del canal
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "MySuperChannel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "ASDASDASD"
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
